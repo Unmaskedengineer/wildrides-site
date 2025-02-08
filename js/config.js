962718dba39b36b116aeff940c52685bf98b8e27
Unmaskedengineer <chimaloski@gmail.com>
2025-02-08 18:15:15 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,8 +1,8 @@
 window._config = {
     cognito: {
-        userPoolId: '', // e.g. us-east-2_uXboG5pAb
-        userPoolClientId: '', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
-        region: '' // e.g. us-east-2
+        userPoolId: 'ID:us-east-1_pSsetfqNu', // e.g. us-east-2_uXboG5pAb
+        userPoolClientId: '5lns8eveh2u5fhk8gsahm99prn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
+        region: '' // e.g. us-east-1
     },
     api: {
         invokeUrl: '' // e.g. https://rc7nyt4tql.execute-api.us-west-2.amazonaws.com/prod',
