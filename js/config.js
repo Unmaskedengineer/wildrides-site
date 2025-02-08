485935ea7dec986f0b6e1eff58b3401b88dbe632
Unmaskedengineer <chimaloski@gmail.com>
2025-02-08 18:20:01 +0100

    Update config.js

--- js/config.js
+++ js/config.js
@@ -1,6 +1,6 @@
 window._config = {
     cognito: {
-        userPoolId: 'ID:us-east-1_pSsetfqNu', // e.g. us-east-2_uXboG5pAb
+        userPoolId: 'us-east-1_pSsetfqNu', // e.g. us-east-2_uXboG5pAb
         userPoolClientId: '5lns8eveh2u5fhk8gsahm99prn', // e.g. 25ddkmj4v6hfsfvruhpfi7n4hv
         region: '' // e.g. us-east-1
     },
